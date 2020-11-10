011b1580c21ac4a70c2011faaf561793e9e45657
Julien ROUSSEAU <jrousseau@doubletrade.com>
2020-11-11 00:46:57 +0100

    chore: add build script

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,2 @@
 declare module "page/page.mjs";
+declare module "@open-wc/building-rollup";
---
+++ rollup.config.ts
@@ -0,0 +1,22 @@
+import merge from "deepmerge";
+import { createSpaConfig } from "@open-wc/building-rollup";
+
+const baseConfig = createSpaConfig({
+  outputDir: "build",
+  injectServiceWorker: false,
+});
+
+export default merge(baseConfig, {
+  input: "./index.html",
+  onwarn(warning) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if (warning.code === "THIS_IS_UNDEFINED") {
+      return;
+    }
+
+    // console.warn everything else
+    console.warn(warning.message);
+  },
+});
